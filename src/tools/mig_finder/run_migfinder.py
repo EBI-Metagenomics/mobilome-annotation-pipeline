624f4d1292794a0e8fcc5f11805afcaf3c1550da
Martin Beracochea <mbc@ebi.ac.uk>
2022-06-24 09:55:58 +0100

    MIG_finder - multi-stage build

--- src/tools/mig_finder/run_migfinder.py
+++ src/tools/mig_finder/run_migfinder.py
@@ -14,8 +14,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-sys.path.append("/install/migfinder-master/migfinder")
-
+import argparse
 import migfinder as mf
-mf.main("/install/migfinder-master/exampleData/mgm4570162.3.050.1.fasta")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate the input.yaml for the CWL pipeline")
+    parser.add_argument("-f", dest="fasta",
+                        required=True,
+                        help="Fasta file")
+    args = parser.parse_args()
+
+    # The output file will be stored in the same path as the provided fasta file
+    mf.main(args.fasta)
