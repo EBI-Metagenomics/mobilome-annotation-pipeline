d09e9cf57a5f3c2717fe7151750dcd2f26daa1b9
Alejandra Escobar <ales@hl-codon-03-04.ebi.ac.uk>
2024-05-02 15:33:43 +0100

    Fix to integronfinder to report all attC sites found

--- bin/python_modules/integronfinder_process.py
+++ bin/python_modules/integronfinder_process.py
@@ -9,46 +9,64 @@ from Bio import SeqIO
 
 
 def integron_parser(mge_data, integron_results, inf_gbks):
-    ### Parsing IntegronFinder output
-    mge_counter = 0
-    attC_site = {}
+    ### Parsing IntegronFinder summary file
+    gbk_files_list = []
     if os.stat(integron_results).st_size > 0:
         with open(integron_results, "r") as input_table:
             next(input_table)
             next(input_table)
             for line in input_table:
-                id_replicon, calin, complete, in0, topology, size = line.rstrip().split(
-                    "\t"
-                )
+                id_replicon, calin, complete, in0, topology, size = line.rstrip().split("\t")
+
+                # Saving gbk file names of complete integrons
                 if int(complete) > 0:
-                    description = "mobile_element_type=complete_integron"
                     gbk_file = id_replicon + ".gbk"
-                    if gbk_file in inf_gbks:
-                        flag = 0
-                        for gb_record in SeqIO.parse(gbk_file, "genbank"):
-                            for feature in gb_record.features:
-                                if feature.type == "integron":
-                                    if (
-                                        feature.qualifiers["integron_type"][0]
-                                        == "complete"
-                                    ):
-                                        flag = 1
-                                        mge_counter += 1
-                                        mge_id = "inf_" + str(mge_counter)
-                                        start = int(feature.location.start)
-                                        if start == 0:
-                                            start = 1
-                                        end = int(feature.location.end)
-                                        coord = (start, end)
-                                        value = (id_replicon, description, coord)
-                                        mge_data[mge_id] = value
-                                if feature.type == "attC":
-                                    if flag == 1:
-                                        start = str(feature.location.start)
-                                        if start == 0:
-                                            start = 1
-                                        end = str(feature.location.end)
-                                        coord = (start, end)
-                                        attC_site[mge_id] = coord
-                                        flag = 0
+                    gbk_files_list.append(gbk_file)
+
+
+    mge_counter = 0
+    attC_site = {}
+    for gbk_file in gbk_files_list:
+        for gb_record in SeqIO.parse(gbk_file, "genbank"):
+
+            # Get indexes for integron records and attC
+            indexes_integron, indexes_attc = [], []
+            for index, feature in enumerate(gb_record.features):
+                if feature.type == "integron":
+                    indexes_integron.append(index)
+                elif feature.type == "attC":
+                    indexes_attc.append(index)
+
+            # process all integrons
+            for index_integron in indexes_integron:
+                integron_feature = gb_record.features[index_integron]
+                if integron_feature.qualifiers["integron_type"][0] == "complete":
+                    mge_counter += 1
+                    mge_id = "inf_" + str(mge_counter)
+                    mge_start = int(integron_feature.location.start)
+                    if mge_start == 0:
+                        mge_start = 1
+                    mge_end = int(integron_feature.location.end)
+                    mge_coord = (mge_start, mge_end)
+                    description = "mobile_element_type=complete_integron"
+                    id_replicon = gbk_file.replace('.gbk','')
+                    value = (id_replicon, description, mge_coord)
+                    mge_data[mge_id] = value
+
+                    print('process integron -> check attC')
+                    print(value)
+                    # check attC
+                    for index_attc in indexes_attc:
+                        attc_feature = gb_record.features[index_attc]
+                        attc_start = str(attc_feature.location.start)
+                        if attc_start == 0:
+                            attc_start = 1
+                        attc_end = str(attc_feature.location.end)
+                        attc_coord = (attc_start, attc_end)
+
+                        if mge_id in attC_site:
+                            attC_site[mge_id].append(attc_coord)
+                        else:
+                            attC_site[mge_id] = [attc_coord]
+
     return (mge_data, attC_site)
