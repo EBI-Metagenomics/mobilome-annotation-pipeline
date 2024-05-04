342b65e707224ae383a17c747898e010e6a5f788
Ekaterina Sakharova <kates@ebi.ac.uk>
2024-05-04 16:56:06 +0100

    ignore negative locations

--- bin/python_modules/integronfinder_process.py
+++ bin/python_modules/integronfinder_process.py
@@ -23,35 +23,10 @@ def integron_parser(mge_data, integron_results, inf_gbks):
                 if int(complete) > 0:
                     gbk_file = id_replicon + ".gbk"
                     gbk_files_list.append(gbk_file)
-    """
-     TODO: fix negative values
-     error: BiopythonParserWarning: Couldn't parse feature location: '-19..3084'. 
-     feature.location = None in those cases
-     !!! VERY BAD SOLUTION - FIX IT !!!
-     fixing cases like : 
-     integron        -19..3084
-     attC            complement(-19..63)
-     Code reads gbk file and replace negative numbers. Saves gbk file to _fixed.gbk.
-    """
-    new_gbk_files_list = []
-    for gbk_file in gbk_files_list:
-        pattern_location = r'\d+\.\.\d+'
-        new_name = os.path.join(os.path.dirname(gbk_file), 'fixed_' + os.path.basename(gbk_file))
-        new_gbk_files_list.append(new_name)
-        with open(gbk_file, 'r') as gbk_file_in, open(new_name, 'w') as file_out:
-            for line in gbk_file_in:
-                match = re.search(pattern_location, line)
-                if match:
-                    pattern_neg = r'-\d+'
-                    # Replace negative values with 0 using re.sub
-                    modified_line = re.sub(pattern_neg, '1', line)
-                    file_out.write(modified_line)
-                else:
-                    file_out.write(line)
 
     mge_counter = 0
     attC_site = {}
-    for gbk_file in new_gbk_files_list:
+    for gbk_file in gbk_files_list:
         for gb_record in SeqIO.parse(gbk_file, "genbank"):
 
             # Get indexes for integron records and attC
@@ -68,13 +43,15 @@ def integron_parser(mge_data, integron_results, inf_gbks):
                 if integron_feature.qualifiers["integron_type"][0] == "complete":
                     mge_counter += 1
                     mge_id = "inf_" + str(mge_counter)
+                    if not integron_feature.location:
+                        continue
                     mge_start = int(integron_feature.location.start)
                     if not mge_start:
                         mge_start = 1
                     mge_end = int(integron_feature.location.end)
                     mge_coord = (mge_start, mge_end)
                     description = "mobile_element_type=complete_integron"
-                    id_replicon = gbk_file.replace('.gbk','').replace('fixed_', '')
+                    id_replicon = gbk_file.replace('.gbk', '')
                     value = (id_replicon, description, mge_coord)
                     mge_data[mge_id] = value
 
