ebb552703b31be3d787d2c6415f5451c2f4af3db
Alejandra Escobar <ales@ebi.ac.uk>
2024-05-25 22:12:11 +0100

    Update gff_mapping.py
    user_mobilome_celan.gff outoput added

--- bin/gff_mapping.py
+++ bin/gff_mapping.py
@@ -11,9 +11,10 @@ import glob
 ##### October 19, 2023
 
 
-def mobilome_parser(mobilome_prokka_extra):
+def mobilome_parser( mobilome_clean ):
     # Parsing the mobilome prediction
     proteins_annot, mobilome_annot = {}, {}
+    extra_proteins = []
 
     source_tools = [
         "ICEfinder",
@@ -31,8 +32,8 @@ def mobilome_parser(mobilome_prokka_extra):
         "mobileOG",
     ]
 
-    if os.stat(mobilome_prokka_extra).st_size > 0:
-        with open(mobilome_prokka_extra, "r") as input_table:
+    if os.stat(mobilome_clean).st_size > 0:
+        with open(mobilome_clean, "r") as input_table:
             for line in input_table:
                 l_line = line.rstrip().split("\t")
                 # Annotation lines have exactly 9 columns
@@ -57,19 +58,18 @@ def mobilome_parser(mobilome_prokka_extra):
                             if att_key in extra_annot:
                                 extra_list.append(attr)
                         if len(extra_list) > 0:
+                            extra_proteins.append(composite_key)
                             extra_val = ";".join(extra_list)
                             proteins_annot[composite_key] = extra_val
 
-    return (proteins_annot, mobilome_annot)
+    return (proteins_annot, mobilome_annot, extra_proteins)
 
 
 # Adding the mobilome predictions to the user file
-def gff_updater(user_gff, proteins_annot, mobilome_annot):
+def gff_updater(user_gff, proteins_annot, mobilome_annot, extra_proteins):
     used_contigs = []
     if os.stat(user_gff).st_size > 0:
-        with open(user_gff, "r") as input_table, open(
-            "user_mobilome_extra.gff", "w"
-        ) as output_table, open("user_mobilome_full.gff", "w") as output_full:
+        with open(user_gff, "r") as input_table, open("user_mobilome_extra.gff", "w") as output_extra, open("user_mobilome_full.gff", "w") as output_full, open("user_mobilome_clean.gff", "w") as output_clean:
             for line in input_table:
                 l_line = line.rstrip().split("\t")
                 # Annotation lines have exactly 9 columns
@@ -82,19 +82,23 @@ def gff_updater(user_gff, proteins_annot, mobilome_annot):
                         used_contigs.append(contig)
                         if contig in mobilome_annot:
                             for mge in mobilome_annot[contig]:
-                                output_table.write(mge + "\n")
+                                output_extra.write(mge + "\n")
                                 output_full.write(mge + "\n")
 
                     composite_val = (contig, start, end, strand)
                     if composite_val in proteins_annot:
                         extra_annot = proteins_annot[composite_val]
-                        output_table.write(line.rstrip() + ";" + extra_annot + "\n")
+                        output_clean.write(line.rstrip() + ";" + extra_annot + "\n")
                         output_full.write(line.rstrip() + ";" + extra_annot + "\n")
+                        if composite_val in extra_proteins:
+                            output_extra.write(line.rstrip() + ";" + extra_annot + "\n")
+
                     else:
                         output_full.write(line.rstrip() + "\n")
 
                 else:
-                    output_table.write(line.rstrip() + "\n")
+                    output_clean.write(line.rstrip() + "\n")
+                    output_extra.write(line.rstrip() + "\n")
                     output_full.write(line.rstrip() + "\n")
 
 def main():
@@ -102,9 +106,9 @@ def main():
         description="This script add the extra annotations to the user GFF file"
     )
     parser.add_argument(
-        "--mobilome_prokka_extra",
+        "--mobilome_clean",
         type=str,
-        help="Mobilome prediction on prokka gff file containing the mobilome proteins with extra annotation only",
+        help="Mobilome prediction on prokka gff file containing the mobilome proteins",
         required=True,
     )
     parser.add_argument(
@@ -117,10 +121,10 @@ def main():
 
     ## Calling functions
     # Storing the mobilome predictions
-    (proteins_annot, mobilome_annot) = mobilome_parser(args.mobilome_prokka_extra)
+    ( proteins_annot, mobilome_annot, extra_proteins ) = mobilome_parser(args.mobilome_clean)
 
     # Adding the mobilome predictions to the user file
-    gff_updater(args.user_gff, proteins_annot, mobilome_annot)
+    gff_updater( args.user_gff, proteins_annot, mobilome_annot, extra_proteins )
 
 
 if __name__ == "__main__":
